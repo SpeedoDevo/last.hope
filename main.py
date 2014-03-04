1ab9f03c98111ad12613e1a803da8d91deca5b12
SpeedoDevo <speedo@devo.hu>
2014-03-04 00:23:18 +0000

    removed unnecessary remaining comments

--- main.py
+++ main.py
@@ -61,12 +61,6 @@ class Game(object):
         reset the game we'd just need to create a new instance of this
         class. """
 
-    # --- Class attributes. 
-    # In this case, all the data we need 
-    # to run our game.
-    
-    # Sprite lists
-    # block_list = None
     all_sprites_list = None
     player = None
     
@@ -74,27 +68,11 @@ class Game(object):
     game_over = False
     # score = 0
     
-    # --- Class methods
-    # Set up the game
     def __init__(self):
         # self.score = 0
         self.game_over = False
-        
-        # Create sprite lists
-        # self.block_list = pygame.sprite.Group()
         self.all_sprites_list = pygame.sprite.Group()
         
-        # Create the block sprites
-        # for i in range(50):
-        #     block = Block()
-        
-        #     block.rect.x = random.randrange(SCREEN_WIDTH)
-        #     block.rect.y = random.randrange(-300, SCREEN_HEIGHT)
-            
-        #     self.block_list.add(block)
-        #     self.all_sprites_list.add(block)
-        
-        # Create the player
         self.player = Player()
         self.all_sprites_list.add(self.player)
 
@@ -120,30 +98,11 @@ class Game(object):
             # Move all the sprites
             self.all_sprites_list.update()
             
-            # # See if the player block has collided with anything.
-            # blocks_hit_list = pygame.sprite.spritecollide(self.player, self.block_list, True)  
-         
-            # # Check the list of collisions.
-            # for block in blocks_hit_list:
-            #     self.score += 1
-            #     print( self.score )
-                
-            # if len(self.block_list) == 0:
-            #     self.game_over = True
                 
     def display_frame(self, screen):
         """ Display everything to the screen for the game. """
         screen.fill(WHITE)
         
-        # if self.game_over:
-        #     #font = pygame.font.Font("Serif", 25)
-        #     font = pygame.font.SysFont("serif", 25)
-        #     text = font.render("Game Over, click to restart", True, BLACK)
-        #     center_x = (SCREEN_WIDTH // 2) - (text.get_width() // 2)
-        #     center_y = (SCREEN_HEIGHT // 2) - (text.get_height() // 2)
-        #     screen.blit(text, [center_x, center_y])
-        
-        #if not self.game_over:
         self.all_sprites_list.draw(screen)
             
         pygame.display.flip()
