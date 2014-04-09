b73d6d99d1d0675b645acd00d0a60e9ece362e8f
oligee <oliver_gee@msn.com>
2014-04-09 13:32:10 +0100

    added score

---
+++ last.hope/constants.py
@@ -0,0 +1,8 @@
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+GREEN    = (   0, 255,   0)
+RED      = ( 255,   0,   0)
+GREY     = ( 100, 100, 100)
+
+SCREEN_WIDTH  = 900
+SCREEN_HEIGHT = 600
