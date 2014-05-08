83804aec224bbaa7a398232c653d532ad2756474
oligee <oliver_gee@msn.com>
2014-05-08 12:02:23 +0100

    add score display using menu style

--- leaderbord.py
+++ leaderbord.py
@@ -1,25 +1,31 @@
 import pygame
 import sys
 import control
-import leaderbord
 from constants import (SCREEN_WIDTH, SCREEN_HEIGHT, RED, GREEN, GREY, BLACK, WHITE)
 
 
 class Menu(pygame.sprite.Sprite):
+
     def __init__(self, screen, clock):
+        pygame.display.set_caption("LAST HOPE -SCOREs-")
         pygame.sprite.Sprite.__init__(self)
         self.screen = screen
         self.clock = clock
-        self.font = pygame.font.Font('image/langdon.otf', 40)
-        self.options = ["start game", "highscores", "quit"]
+        self.font = pygame.font.Font('image/langdon.otf', 50)
+        self.options = ["menu","quit"]
         self.screenRect = (0,0,SCREEN_WIDTH,SCREEN_HEIGHT)
         self.pos = 0
         self.bg = control.Background()
-
+        self.file = open( "Scores.txt", "r" )
+        self.array = []
+        for line in self.file:
+            self.array.append(int(line))
+        self.file.close()   
     def draw(self,screen):
         self.bg.update()
         screen.blit(self.bg.image,self.bg.rect)
         for i in range(len(self.options)):
+            colorScore = GREY
             if i == self.pos:
                 color = WHITE
             else:
@@ -27,13 +33,19 @@ class Menu(pygame.sprite.Sprite):
             self.text = self.font.render(self.options[i], True, color)
             self.textrect = self.text.get_rect()
             self.textrect.center = (SCREEN_WIDTH/2,(200+i*45))
-            self.screen.blit(self.text,self.textrect)
+            self.screen.blit(self.text,self.textrect) 
+        for i in range(0,5):
+            self.score1 = self.font.render("--" + str(self.array[i]) + "--", True, colorScore)
+            self.score1rect = self.score1.get_rect()
+            self.score1rect.center = (SCREEN_WIDTH/2,(350+i*45))
+            self.screen.blit(self.score1,self.score1rect)        
+
         # screen.blit(self.image,self.screenRect)
         pygame.display.update()
 
 
     def update(self):
-        self.pos %= 3
+        self.pos %= 2
         for event in pygame.event.get():
             print(event)
             if event.type == pygame.QUIT:
@@ -54,8 +66,5 @@ class Menu(pygame.sprite.Sprite):
             if selection == 0:
                 return
             elif selection == 1:
-                scoreMenu = leaderbord.Menu(self.screen,self.clock)
-                scoreMenu.run()                
-            elif selection == 2:
                 pygame.quit()
                 sys.exit(0)
